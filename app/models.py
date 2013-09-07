5c19cfa7311034d8c1999be34ab1947ba84f088d
mccormickt12 <cal@cal-laptop.(none)>
2013-09-07 10:50:37 -0700

    project form

--- app/models.py
+++ app/models.py
@@ -36,8 +36,9 @@ class User(db.Model):
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64))
+    description = db.Column(db.String(400))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # team = db.relationship('User', backref = 'project', lazy = 'dynamic')
+    team = db.relationship('User', backref = 'project', lazy = 'dynamic')
 
     def __repr__(self):
         return '<Project %r>' % (self.name)
--- app/views.py
+++ app/views.py
@@ -25,19 +25,39 @@ def create():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User(name=form.name.data, email=form.email.data,
-                    password=md5(form.password.data).hexdigest())
+                    password=md5(form.password.data).hexdigest(),
+                    major=form.major.data, minor=form.minor.data,
+                    year=form.year.data)
         db.session.add(user)
         db.session.commit()
         flash('Thanks for registering')
-        return redirect(url_for('create'))
+        return redirect(url_for('example'))
     return render_template('create.html', active="create", form=form)
+
+@app.route('/new/project' methods=['POST', 'GET'])
+def create():
+    form = ProjectForm(request.form)
+    if request.method == 'POST' and form.validate():
+        project = Project(name=form.name.data, description=form.description.data)
+        db.session.add(project)
+        db.session.commit()
+        return redirect(url_for('example'))
+    return render_template('new_project.html', active="project", form=form)
+
 
 class RegistrationForm(Form):
     name = TextField('Name', [validators.Length(min=4, max=25)])
     email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    major = TextField('Major', [validators.Length(min=2, max=30)])
+    minor = TextField('Minor', [validators.Length(min=2, max=30)])
+    year = TextField('Year', [validators.Length(min=2, max=30)])
     password = PasswordField('New Password', [
         validators.Required(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password')
     accept_tos = BooleanField('I accept the TOS', [validators.Required()])
+
+class ProjectForm(Form):
+    name = TextField('Name', [validators.Length(min=4, max=25)])
+    description = TextArea('Description', validators.Length(min=10, max=400))
