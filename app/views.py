5ef08995e102063a1ab2e43104103f09be0beca8
mccormickt12 <mccormickt12@gmail.com>
2013-09-15 16:11:12 -0700

    delete button for project

--- app/views.py
+++ app/views.py
@@ -204,10 +204,16 @@ def proj_page(uname, proj):
         if project.first():
             project = project[0]
             if request.method == 'POST':
-                if project.addMember(session.get('user_id')):
-                    flash("You have now joined this project")
-                else: 
-                    flash("Already joined this project")
+                if request.form.get('join'):
+                    if project.addMember(session.get('user_id')):
+                        flash("You have now joined this project")
+                    else: 
+                        flash("Already joined this project")
+                elif request.form.get('remove'):
+                    db.session.delete(project)
+                    db.session.commit()
+                    flash("Project successfully deleted")
+                    return redirect(url_for('discover'))
             return render_template('project.html', user = user, project = project, logged_in=session.get('logged_in'),
              me=session.get('user_id'), members=project.getMembers())
     return render_template('error.html')
